74626ad50390c7d0aae7e7b07935223c1ec1c00c
liumapp <liumapp.com@gmail.com>
2018-05-05 14:19:26 +0800

    get redis image in docker

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # spring-session-redis-in-docker
 Running Spring Session Redis in Docker . 
+
+## install and run redis in docker 
+
--- src/main/java/com/liumapp/docker/redis/SpringSessionRedisDemoApplication.java
+++ src/main/java/com/liumapp/docker/redis/SpringSessionRedisDemoApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
+/**
+ * @author liumapp
+ * @file SpringSessionRedisDemoApplication.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 5/5/18
+ */
 @SpringBootApplication
 @Import({
 		Main.class,
